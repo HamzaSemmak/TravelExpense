acf2425dbde1c27482839e5b2f269f955c5c718d
hsemmak <hsemmak@sorec.ma>
2025-03-15 00:52:07 +0000

    Working on Progress

---
+++ travelExpense/Controllers/TravelController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using travelExpense.Data;
+
+namespace travelExpense.Controllers
+{
+    public class TravelController : Controller
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+
+        public TravelController(ApplicationDbContext applicationDbContext)
+        {
+            this.applicationDbContext = applicationDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var travels = applicationDbContext.Travels.Include(t => t.Category).ToList();
+            return View(travels);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            var categorys = await applicationDbContext.Categories.ToListAsync();
+            return View(categorys);
+        }
+    }
+}
